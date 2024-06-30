70ffe384328c597b00f921812076b5d94857f8d0
Teja Prakash <tejaprakash@MacBook-Air.local>
2024-06-30 12:11:22 +0530

    Second commit

--- app/login/page.js
+++ app/login/page.js
@@ -4,6 +4,7 @@ import styles from "../ui/login/login.module.css";
 export default function LoginPage() {
     return (
         <div className={styles.container}>
+            <div>Hello</div>
             <LoginForm />
         </div>
     )
